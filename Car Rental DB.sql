8fd1ed80e2fcc59bf4fda41506b3029239c7b06e
Musyoki-Mutinda <leone.mutinda@strathmore.edu>
2025-10-06 15:22:36 +0300

    Inserted Values

--- Car Rental DB.sql
+++ Car Rental DB.sql
@@ -93,3 +93,98 @@ CREATE TABLE Reservation (
     CONSTRAINT FK_Reservation_Customer FOREIGN KEY (CustomerID) REFERENCES Customer(CustomerID)
 );
 GO
+
+
+INSERT INTO Customer (FirstName, LastName, Email, PhoneNumber, Address)
+VALUES
+('John', 'Muiruri', 'johnmuiruri@yahoo.com', '0723769461', 'Nairobi'),
+('Janet', 'Wanjiru', 'janetwanjiru@gmail.com', '0700468752', 'Mombasa'),
+('Alexis', 'Kamau', 'alexiskamau@hotmail.com', '0701357608', 'Nakuru'),
+('Mary', 'Otieno', 'maryotieno@yahoo.com', '0769653874', 'Kisumu'),
+('Peter', 'Mwangi', 'petermwangi@gmail.com', '0722694705', 'Eldoret'),
+('Linda', 'Mutua', 'lindamutua@icloud.com', '0728759763', 'Thika'),
+('Brian', 'Musanga', 'briannjoroge@gmail.com', '0729057907', 'Naivasha');
+
+
+INSERT INTO Car (CarModel, Manufacturer, Year, Color, RentalRate, Availability)
+VALUES
+('Corolla', 'Toyota', 2020, 'White', 3500.00, 1),
+('CX-5', 'Mazda', 2021, 'Blue', 5000.00, 1),
+('X-Trail', 'Nissan', 2019, 'Black', 4000.00, 1),
+('Civic', 'Honda', 2022, 'Red', 4500.00, 1),
+('Ranger', 'Ford', 2018, 'Grey', 6000.00, 1),
+('Land Cruiser', 'Toyota', 2021, 'Black', 8000.00, 1),
+('Swift', 'Suzuki', 2020, 'Silver', 3000.00, 1);
+
+
+INSERT INTO Booking (CarID, CustomerID, RentalStartDate, RentalEndDate, TotalAmount)
+VALUES
+(1, 1, '2025-10-01', '2025-10-05', 14000.00),
+(2, 2, '2025-09-20', '2025-09-25', 25000.00),
+(3, 3, '2025-09-10', '2025-09-15', 20000.00),
+(4, 4, '2025-08-01', '2025-08-10', 45000.00),
+(5, 5, '2025-07-15', '2025-07-20', 30000.00),
+(6, 6, '2025-06-01', '2025-06-03', 16000.00),
+(7, 7, '2025-05-05', '2025-05-07', 6000.00);
+
+
+INSERT INTO Payment (BookingID, PaymentDate, Amount, PaymentMethod)
+VALUES
+(1, '2025-10-01', 14000.00, 'M-Pesa'),
+(2, '2025-09-20', 25000.00, 'Credit Card'),
+(3, '2025-09-10', 20000.00, 'Cash'),
+(4, '2025-08-01', 45000.00, 'Bank Transfer'),
+(5, '2025-07-15', 30000.00, 'M-Pesa'),
+(6, '2025-06-01', 16000.00, 'Credit Card'),
+(7, '2025-05-05', 6000.00, 'Cash');
+
+
+INSERT INTO Location (CarID, LocationName, Address, ContactNumber)
+VALUES
+(1, 'Nairobi Branch', 'Moi Avenue', '0701065981'),
+(2, 'Mombasa Branch', 'Diani Road', '0701094872'),
+(3, 'Nakuru Branch', 'Kenyatta Avenue', '0764897503'),
+(4, 'Kisumu Branch', 'Jomo Kenyatta Highway', '0711643004'),
+(5, 'Eldoret Branch', 'Oloo Street', '0721040075'),
+(6, 'Thika Branch', 'Thika Road', '0701874636'),
+(7, 'Naivasha Branch', 'Mama Ngina Street', '0747398507');
+
+
+INSERT INTO Insurance (CarID, InsuranceProvider, PolicyNumber, StartDate, EndDate)
+VALUES
+(1, 'Jubilee', 'POL001', '2025-01-01', '2025-12-31'),
+(2, 'Britam', 'POL002', '2025-02-01', '2026-01-31'),
+(3, 'Madison', 'POL003', '2025-03-01', '2026-02-28'),
+(4, 'APA', 'POL004', '2025-04-01', '2026-03-31'),
+(5, 'CIC', 'POL005', '2025-05-01', '2026-04-30'),
+(6, 'UAP', 'POL006', '2025-06-01', '2026-05-31'),
+(7, 'Heritage', 'POL007', '2025-07-01', '2026-06-30');
+
+
+INSERT INTO Maintenance (CarID, MaintenanceDate, Description, Cost)
+VALUES
+(1, '2025-05-10', 'Oil change', 3000.00),
+(2, '2025-04-20', 'Brake replacement', 12000.00),
+(3, '2025-03-15', 'Tire replacement', 8000.00),
+(4, '2025-02-10', 'Engine tune-up', 15000.00),
+(5, '2025-01-05', 'Suspension repair', 20000.00),
+(6, '2024-12-01', 'Air filter change', 2500.00),
+(7, '2024-11-15', 'Battery replacement', 10000.00);
+
+
+INSERT INTO Reservation (CarID, CustomerID, ReservationDate, PickupDate, ReturnDate)
+VALUES
+(1, 1, '2025-09-25', '2025-10-01', '2025-10-05'),
+(2, 2, '2025-09-15', '2025-09-20', '2025-09-25'),
+(3, 3, '2025-09-05', '2025-09-10', '2025-09-15'),
+(4, 4, '2025-07-25', '2025-08-01', '2025-08-10'),
+(5, 5, '2025-07-10', '2025-07-15', '2025-07-20'),
+(6, 6, '2025-05-25', '2025-06-01', '2025-06-03'),
+(7, 7, '2025-05-01', '2025-05-05', '2025-05-07');
+GO
+
+
+UPDATE Car SET Availability = 0 WHERE CarID = 1;
+
+DELETE FROM Payment WHERE BookingID = 1;
+DELETE FROM Booking WHERE BookingID = 1;
